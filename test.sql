3b92fb9f6091600388828bc804ebc4bdef22d236
Venkat Rao <venkat05091990@gmail.com>
2022-11-18 12:27:52 +0530

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,16 @@
+DECLARE @json NVARCHAR(2048) = N'{
+   "String_value": "John",
+   "DoublePrecisionFloatingPoint_value": 45,
+   "DoublePrecisionFloatingPoint_value": 2.3456,
+   "BooleanTrue_value": true,
+   "BooleanFalse_value": false,
+   "Null_value": null,
+   "Array_value": ["a","r","r","a","y"],
+   "Object_value": {"obj":"ect"}
+}';
+
+SELECT * FROM OpenJson(@json) 
+			WITH	(   
+							ID   nvarchar(200) '$.String_value',
+							Quantity   nvarchar(200) '$.DoublePrecisionFloatingPoint_value'
+					) 
